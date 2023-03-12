54a95af5a0e31371b658d38ccc20f9211bf8ecb3
PickleEric <60155510+PickleEric@users.noreply.github.com>
2023-03-12 12:34:06 -0500

    combine register and login pages in to one component

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,32 @@
-import React from "react";
-import firebase from "./firebase";
+import React, { useState } from "react";
+import { auth, firestore } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const LoginLogout = () => {
-  const [user, loading, error] = useAuthState(firebase.auth());
+const LoginLogoutRegistration = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
 
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
+    auth.signInWithPopup(provider);
   };
 
   const signOut = () => {
-    firebase.auth().signOut();
+    auth.signOut();
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      await firestore.collection("users").doc(user.uid).set({
+        displayName,
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   if (loading) {
@@ -24,12 +40,32 @@ const LoginLogout = () => {
   return (
     <div>
       {user ? (
-        <button onClick={signOut}>Sign out</button>
+        <div>
+          <p>Welcome, {user.displayName}</p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
       ) : (
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
+        <div>
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Email:
+              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <label>
+              Password:
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <label>
+              Display Name:
+              <input type="text" value={displayName} onChange={(e) => setDisplayName(e.target.value)} />
+            </label>
+            <button type="submit">Register</button>
+          </form>
+        </div>
       )}
     </div>
   );
 };
 
-export default LoginLogout;
+export default LoginLogoutRegistration;

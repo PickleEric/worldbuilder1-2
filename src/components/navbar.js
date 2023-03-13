d989cd013cafe43870dead10e9a96820996dabf9
PickleEric <60155510+PickleEric@users.noreply.github.com>
2023-03-13 13:41:35 -0500

    moved code from index to navbar.js

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+//Array of pages
+
+const pageLinks = [
+    {
+      text: "Page 2",
+      url: "page-2",
+      badge: false,
+      description:
+        "A simple example of linking to another page within a Gatsby site",
+    },
+    { text: "TypeScript", url: "using-typescript" },
+    { text: "Server Side Rendering", url: "using-ssr" },
+    { text: "Deferred Static Generation", url: "using-dsg" },
+  ]
+const navbar = () =>{
+    PageLinks.map((link, i) => (
+    <React.Fragment key={link.url}>
+      <Link to={link.url}>{link.text}</Link>
+      {i !== samplePageLinks.length - 1 && <> · </>}
+    </React.Fragment>
+  ))}
+
+  export default navbar
